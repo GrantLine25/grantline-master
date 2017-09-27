37e2a0ad23de38fac37a170a8f4563eecce1c946
Admin <admin@coin.com>
2017-09-28 00:22:33 +0200

    grantline
    grantline

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define grantline-qt message queue name
+#define BITCOINURI_QUEUE_NAME "grantlineURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
